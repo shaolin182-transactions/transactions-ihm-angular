f7e0c1d8c704545722a0c2ebaa03577a74491026
Julien Girard <girard.julien@gmail.com>
2025-05-06 21:59:24 +0200

    ci: add step for running tests

--- karma.conf.js
+++ karma.conf.js
@@ -22,7 +22,7 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['mocha'],
+    reporters: ['mocha', 'coverage-istanbul'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
